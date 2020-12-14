5b8f9d4ea9f7cf4e5b0a121d6243a19c5c33e2de
wolfadex <wolfadex@gmail.com>
2020-12-13 20:45:36 -0700

    Remove debug

--- 2020/lang-haskell/src/Day12.hs
+++ 2020/lang-haskell/src/Day12.hs
@@ -1,7 +1,5 @@
 module Day12 (solve1, solve2) where
 
-import Debug.Trace
-
 solve1 :: String -> Int
 solve1 = manhattenDistance startPos . navigate (1, 0) startPos . lines
   where
@@ -15,9 +13,9 @@ manhattenDistance (x1, y1) (x2, y2) =
   abs (x1 - x2) + abs (y1 - y2)
 
 navigate :: (Int, Int) -> (Int, Int) -> [String] -> (Int, Int)
-navigate _ endPos [] = Debug.Trace.traceShow endPos endPos
+navigate _ endPos [] = endPos
 navigate (xf, yf) (xp, yp) (dir:rest) =
-  case Debug.Trace.traceShow (xf, yf, xp, yp, dir) dir of
+  case dir of
     'N' : amount -> navigate (xf, yf) (xp, yp + read amount) rest
     'S' : amount -> navigate (xf, yf) (xp, yp - read amount) rest
     'E' : amount -> navigate (xf, yf) (xp + read amount, yp) rest
@@ -28,7 +26,7 @@ navigate (xf, yf) (xp, yp) (dir:rest) =
         (0, -1) -> navigate (xf, yf) (xp, yp - read amount) rest -- South
         (1, 0) -> navigate (xf, yf) (xp + read amount, yp) rest -- East
         (-1, 0) -> navigate (xf, yf) (xp - read amount, yp) rest -- West
-        _ -> Debug.Trace.trace "shouldn't be here either" navigate (xf, yf) (xp, yp) rest
+        _ -> navigate (xf, yf) (xp, yp) rest
     'R' : amount -> navigate (rotate (xf, yf) (read amount)) (xp, yp) rest
     'L' : amount -> navigate (rotate (xf, yf) (-(read amount))) (xp, yp) rest
 
@@ -44,4 +42,4 @@ rotate (x, y) amount =
     (90, 0, -1) -> (-1 * signum amount, 0)
     (270, 0, 1) -> (-1 * signum amount, 0)
     (270, 0, -1) -> (1 * signum amount, 0)
-    _ -> Debug.Trace.trace "shouldn't be here" (x, y)
+    _ -> (x, y)
