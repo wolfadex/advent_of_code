a5070b428a37b4ead88fb3c28e2d6620bcb47119
wolfadex <wolfadex@gmail.com>
2021-12-12 07:56:25 -0600

    2021 day1 part2 elm

--- 2021/lang-elm/src/Day01.elm
+++ 2021/lang-elm/src/Day01.elm
@@ -30,5 +30,39 @@ program =
                             |> String.fromInt
         , part2 =
             \content ->
-                "carl"
+                content
+                    |> String.lines
+                    |> List.filterMap String.toInt
+                    |> slidingWindow
         }
+
+
+slidingWindow : List Int -> String
+slidingWindow depths =
+    case depths of
+        a :: b :: c :: rest ->
+            slidingWindowHelper (a + b + c) (b :: c :: rest) 0
+
+        _ ->
+            "-1"
+
+
+slidingWindowHelper : Int -> List Int -> Int -> String
+slidingWindowHelper previousDepth depths increases =
+    case depths of
+        a :: b :: c :: rest ->
+            let
+                depth =
+                    a + b + c
+
+                newIncreases =
+                    if depth > previousDepth then
+                        increases + 1
+
+                    else
+                        increases
+            in
+            slidingWindowHelper depth (b :: c :: rest) newIncreases
+
+        _ ->
+            String.fromInt increases
